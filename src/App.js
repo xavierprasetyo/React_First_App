d6a9076fe9120fadb332d31efbd03cc36c1f8e98
xavierprasetyo <xavierprasetyo89@gmail.com>
2020-03-09 07:15:00 +0700

    hae

--- src/App.js
+++ src/App.js
@@ -3,10 +3,29 @@ import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos : [
+      {
+        id :1,
+        title : 'Bandung',
+        done : false
+      },
+      {
+        id :2,
+        title : 'Jakarta',
+        done : false
+      },
+      {
+        id :1,
+        title : 'Malang',
+        done : true
+      }
+    ]
+  }
   render(){
     return (
       <div className="App">
-        <Todos/>
+        <Todos todos = {this.state.todos}/>
       </div>
     );
   }
